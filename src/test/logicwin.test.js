63a13f18568a68720131fc4506dccd219a9c8d93
Darya Ivanovich <darya.ivanovich@itechart-group.com>
2017-06-01 09:43:23 +0300

    add tests

--- src/test/logicwin.test.js
+++ src/test/logicwin.test.js
@@ -1,80 +1,112 @@
-// import detectWinner from '../logic/detectWinner';
-//
-// describe('winDiagonal', () => {
-//     it('should win diagonal X', () => {
-//         const state = {
-//             board: [
-//                 ['X', '0', 'X'],
-//                 ['0', 'X', '0'],
-//                 ['X', 'X', '0']
-//             ],
-//             symbol: 'X',
-//             gameEnd: false,
-//             win: false
-//         };
-//
-//         const expectValue = {
-//             win: true,
-//             stillPlay: true
-//         };
-//
-//         expect(detectWinner(state)).toEqual(expectValue);
-//     });
-// });
+import detectWinner from '../logic/detectWinner';
 
-// describe('winRow', () => {
-//     it('should win row', () => {
-//         const board = [
-//                 ['X', 'X', 'X'],
-//                 ['0', 'X', ''],
-//                 ['', '', 'X']
-//         ];
-//         expect(detectWinner(board, 'X')).toEqual(true);
-//     });
-// });
-//
-// describe('winCell', () => {
-//     it('should win cell', () => {
-//         const board = [
-//                 ['', 'X', '0'],
-//                 ['0', 'X', ''],
-//                 ['', 'X', 'X']
-//         ];
-//         expect(detectWinner(board, 'X')).toEqual(true);
-//     });
-// });
-//
-// describe('winRevertDiagonal', () => {
-//     it('should win revert diagonal', () => {
-//         const board = [
-//                 ['', 'X', '0'],
-//                 ['0', '0', ''],
-//                 ['0', '0', 'X']
-//         ];
-//         expect(detectWinner(board, '0')).toEqual(true);
-//     });
-// });
-//
-// describe('nobodyWin', () => {
-//     it('should return false (nobody win)', () => {
-//         const board = [
-//                 ['0', 'X', '0'],
-//                 ['0', '0', 'X'],
-//                 ['X', '0', 'X']
-//         ];
-//         expect(detectWinner(board, '0')).toEqual(false);
-//     });
-// });
-//
-//
-// describe('win in 4 cells', () => {
-//     it('should return false (nobody win)', () => {
-//         const board = [
-//             ['X', 'X', '0', '0'],
-//             ['0', 'X', 'X', 'X'],
-//             ['X', '0', 'X', 'X'],
-//             ['X', '0', 'X', 'X']
-//         ];
-//         expect(detectWinner(board, 'X')).toEqual(true);
-//     });
-// });
+describe('winDiagonal', () => {
+    it('should win diagonal X', () => {
+        const state = {
+            board: [
+                ['X', '0', 'X'],
+                ['0', 'X', '0'],
+                ['X', 'X', '0']
+            ],
+            symbol: 'X',
+            gameEnd: false,
+            win: false
+        };
+
+        const expectValue = {
+            win: true,
+            stillPlay: false
+        };
+
+        expect(detectWinner(state)).toEqual(expectValue);
+    });
+});
+
+describe('noEmptySpace', () => {
+    it('No empty space, nobody win', () => {
+        const state = {
+            board: [
+                ['X', '0', 'X'],
+                ['0', '0', '0'],
+                ['X', 'X', '0']
+            ],
+            symbol: 'X',
+            gameEnd: false,
+            win: false
+        };
+
+        const expectValue = {
+            win: false,
+            stillPlay: false
+        };
+
+        expect(detectWinner(state)).toEqual(expectValue);
+    });
+});
+
+describe('winRow', () => {
+    it('Win row, still could play', () => {
+        const state = {
+            board: [
+                ['0', '0', '0'],
+                ['0', '0', ''],
+                ['', 'X', 'X']
+            ],
+            symbol: '0',
+            gameEnd: false,
+            win: false
+        };
+
+        const expectValue = {
+            win: true,
+            stillPlay: true
+        };
+
+        expect(detectWinner(state)).toEqual(expectValue);
+    });
+});
+
+describe('wrongSymbol', () => {
+    it('wrongSymbol', () => {
+        const state = {
+            board: [
+                ['0', '0', ')'],
+                ['0', '0', ''],
+                ['', 'X', 'X']
+            ],
+            symbol: '0',
+            gameEnd: false,
+            win: false
+        };
+
+        const expectValue = {
+            win: false,
+            stillPlay: true
+        };
+
+        expect(detectWinner(state)).toEqual(expectValue);
+    });
+});
+
+
+describe('wrongSymbol', () => {
+    it('wrongSymbol', () => {
+        const state = {
+            board: [
+                ['0', '0', ''],
+                ['0', '0', ''],
+                ['X', 'X', 'X']
+            ],
+            symbol: 'X',
+            gameEnd: false,
+            win: false
+        };
+
+        const expectValue = {
+            win: true,
+            stillPlay: true
+        };
+
+        expect(detectWinner(state)).toEqual(expectValue);
+    });
+});
